c0142a023c2c643e41107ed8fe8f45248a6df761
abrarabedin001 <abrarabedin001@gmail.com>
2023-09-10 14:19:35 +0600

    added the feautures section

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -28,43 +28,46 @@ export default function Home() {
             </div>
             <div className="bg-slate-300 h-[300px] w-[500px] grid gird-cols-1 place-items-start p-5">
               <Image
-                src="/Features/Object/call-management.png"
-                alt="call-management"
+                src="/Features/Object/voice-to-text.png"
+                alt="Voice-to-Text Transcription"
                 width={80}
                 height={80}
                 className="w-[80px] h-[80px] mt-5"
               />
               <div className="text-left">
-                <H3 className="leading-[1rem]">Call Management</H3>
-                <p>Make and Receive Calls Directly From the App</p>
+                <H3 className="leading-[1rem]">Voice-to-Text Transcription</H3>
+                <p>Never Miss a Detail</p>
               </div>
             </div>
           </div>
           <div className="pt-9">
             <div className="bg-slate-300 h-[300px] w-[500px] grid gird-cols-1 place-items-start p-5">
               <Image
-                src="/Features/Object/call-management.png"
-                alt="call-management"
+                src="/Features/Object/crm-integration.png"
+                alt="CRM Integration"
                 width={80}
                 height={80}
                 className="w-[80px] h-[80px] mt-5"
               />
               <div className="text-left">
-                <H3 className="leading-[1rem]">Call Management</H3>
-                <p>Make and Receive Calls Directly From the App</p>
+                <H3 className="leading-[1rem]">CRM Integration</H3>
+                <p>AI-Powered Data for Every Customer Interaction</p>
               </div>
             </div>
+
             <div className="bg-slate-300 h-[300px] w-[500px] grid gird-cols-1 place-items-start p-5">
               <Image
-                src="/Features/Object/call-management.png"
-                alt="call-management"
+                src="/Features/Object/actionable.png"
+                alt="Actionable Insights and Summaries"
                 width={80}
                 height={80}
                 className="w-[80px] h-[80px] mt-5"
               />
               <div className="text-left">
-                <H3 className="leading-[1rem]">Call Management</H3>
-                <p>Make and Receive Calls Directly From the App</p>
+                <H3 className="leading-[2rem]">
+                  Actionable Insights and Summaries
+                </H3>
+                <p>AI-Powered Data for Every Customer Interaction</p>
               </div>
             </div>
           </div>
